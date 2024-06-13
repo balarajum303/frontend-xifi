dc80c4153420f5636dfc3cbd510f58493c5003ea
balarajum303 <balarajum303@gmail.com>
2024-06-14 00:02:14 +0530

    updated

--- src/layouts/dashboard/common/FileUpload.jsx
+++ src/layouts/dashboard/common/FileUpload.jsx
@@ -3,6 +3,7 @@ import { Row, Col, Card, Button, CardBody, Container } from "reactstrap";
 import PropTypes from 'prop-types';
 import api from "src/components/Common/api";
 import { CATEGORY_API } from "src/components/Common/apiConfig";
+import { Box, Typography } from "@mui/material";
 
 const FileUpload = () => {
     const [userProfileData, setUserProfileData] = useState({
@@ -37,20 +38,34 @@ const FileUpload = () => {
 
     return (
         <div className="page-content">
+
             <Container fluid>
+
                 <Row>
                     <Col>
                         <Card>
+                            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '18px' }}>
+                                <Card style={{ border: 'none' }}>
+                                    <CardBody style={{ padding: '10px', border: '1px solid silver', borderRadius: '33px',boxShadow:"1px 1px 1px 1px silver" }}>
+                                        <Typography variant="h6" align="center">
+                                            Click on a voice to hear it
+                                        </Typography>
+                                    </CardBody>
+                                </Card>
+                            </div>
                             <CardBody>
-                                <div className="table-rep-plugin">
+                                <div className="table-rep-plugin" style={{marginTop:"18px"}}>
+
                                     <div className="table-responsive mb-0" data-pattern="priority-columns">
+
+
                                         <div>
                                             {userProfileData.profileUrl && (
-                                                <img 
-                                                    src={`${BASE_URL}${userProfileData.profileUrl}`} 
-                                                    alt="Profile" 
-                                                    style={{ maxWidth: '100%' }} 
-                                                    
+                                                <img
+                                                    src={`${BASE_URL}${userProfileData.profileUrl}`}
+                                                    alt="Profile"
+                                                    style={{ maxWidth: '100%' }}
+
                                                 />
                                             )}
                                             {userProfileData.voiceUrl && (
@@ -63,7 +78,7 @@ const FileUpload = () => {
                                     </div>
                                 </div>
                                 <div className="d-flex justify-content-center gap-3">
-                                    <Button color="danger" onClick={clearForm} className="me-2">
+                                    <Button color="danger" onClick={clearForm} className="me-2" style={{marginTop:"18px"}}>
                                         Cancel
                                     </Button>
                                 </div>
